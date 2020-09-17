70913f0dc46ad65ef1130b2007536b9122ec5d12
Lixandre <alexikedamucenic@gmail.com>
2020-09-17 16:22:28 -0300

    Aula 3

--- Plataforma Alexandre/Assets/Player_movement.cs
+++ Plataforma Alexandre/Assets/Player_movement.cs
@@ -7,10 +7,14 @@ public class Player_movement : MonoBehaviour
     public float spd;
     public float jumpF;
     Rigidbody2D rig;
+    Animator anim;
+    public bool isjump;
+
     // Start is called before the first frame update
     void Start()
     {
         rig = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -24,12 +28,46 @@ public class Player_movement : MonoBehaviour
     {
         Vector3 movement = new Vector3(Input.GetAxis("Horizontal"),0f,0f);
         transform.position += movement * Time.deltaTime * spd;
+        //andar para direita
+        if (Input.GetAxis("Horizontal") > 0)
+        {
+            anim.SetBool("Walk", true);
+            transform.eulerAngles = new Vector3(0f, 0f, 0f);
+        }
+        //andar para esquerda
+        if (Input.GetAxis("Horizontal") < 0)
+        {
+            anim.SetBool("Walk", true);
+            transform.eulerAngles = new Vector3(0f, 180f, 0f);
+        }
+        if (Input.GetAxis("Horizontal") == 0)
+        {
+            anim.SetBool("Walk", false);
+        }
     }
     void jump()
     { 
-        if (Input.GetButtonDown("Jump"))
+        if (Input.GetButtonDown("Jump") && !isjump) 
         {
             rig.AddForce(new Vector2(0f,jumpF),ForceMode2D.Impulse);
+            anim.SetBool("Jump", true);
+        }
+
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.layer == 8)
+        {
+            isjump = false;
+            anim.SetBool("Jump", false);
         }
     }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.layer == 8)
+        {
+            isjump = true;
+        }
+    }
+
 }
