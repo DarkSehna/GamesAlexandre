2c1ceb301cb42492133fff7ef959cadfeace2bec
Lixandre <alexikedamucenic@gmail.com>
2020-10-29 15:33:36 -0300

    Aula 9
    Animação e movimento

---
+++ Plataforma Alexandre/Assets/Scripts/Player_move.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_move : MonoBehaviour
+{
+    [Header("Horizontal Movement")]
+    public float moveSpeed = 2f;
+    public Vector2 direction;
+
+    [Header("Components")]
+    public Rigidbody2D rb;
+    public Animator animator;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        direction = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+
+    }
+
+    private void FixedUpdate()
+    {
+        moveCharacter(direction.x);
+    }
+
+    void moveCharacter(float horizontal)
+    {
+        rb.AddForce(Vector2.right * horizontal * moveSpeed);
+
+    }
+}
+
