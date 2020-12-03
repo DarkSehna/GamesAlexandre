9ffc6273266ed6b0287760d0875bd97ea792c3e4
Lixandre <alexikedamucenic@gmail.com>
2020-12-03 15:55:55 -0300

    Aula 14
    Rascunho do mapa e Diálogo.

---
+++ Plataforma Alexandre/Assets/Scripts/Dialogues/DialogueManager.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueManager : MonoBehaviour
+{
+    private Queue<string> sentences;
+    public Text nameText;
+    public Text dialogueText;
+    public Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sentences = new Queue<string>();
+    }
+
+    public void startDialogue(Dialogue dialogue)
+    {
+        animator.SetBool("isOpen",true);
+        nameText.text = dialogue.name;
+        sentences.Clear();
+        foreach (string sentence in dialogue.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+        displayNextSentence();
+    }
+
+    public void displayNextSentence()
+    {
+        if (sentences.Count == 0)
+        {
+            endDialogue();
+            return;
+        }
+        string sentence = sentences.Dequeue();
+        StopAllCoroutines();
+        StartCoroutine(typeSentence(sentence));
+    }
+
+    IEnumerator typeSentence(string sentence)
+    {
+        dialogueText.text = "";
+        foreach (char letter in sentence.ToCharArray())
+        {
+            dialogueText.text += letter;
+            yield return null;
+        }
+    }
+
+    void endDialogue()
+    {
+        animator.SetBool("isOpen", false);
+    }
+
+}
