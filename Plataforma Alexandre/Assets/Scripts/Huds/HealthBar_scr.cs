7646635f478d3424f11103392dea7801e35ab6f1
Lixandre <alexikedamucenic@gmail.com>
2021-05-19 15:09:43 -0300

    Slime 15
    Hud (Health Bar)

---
+++ Plataforma Alexandre/Assets/Scripts/Huds/HealthBar_scr.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar_scr : MonoBehaviour
+{
+    #region Public Variables
+    public Slider slider;
+    #endregion
+
+    public void setHealth(int health)
+    {
+        slider.value = health;
+    }
+
+    public void setMaxHealth(int health)
+    {
+        slider.maxValue = health;
+        slider.value = health;
+    }
+}
