f7dae8e553532ee03c8cd63c1725873ae0955cf1
Lixandre <alexikedamucenic@gmail.com>
2021-05-26 15:02:46 -0300

    Slime 16
    Menu de Pausa

---
+++ Plataforma Alexandre/Assets/Scripts/Huds/PauseMenu.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    #region Public Variables
+    public GameObject pauseMenu;
+    public bool gameIsPaused;
+    #endregion
+
+    private void Update()
+    {
+        if (Input.GetKeyDown("escape"))
+        {
+            if (gameIsPaused)
+            {
+                Unpause();
+            }
+            else 
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Pause()
+    {
+
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+        gameIsPaused = true;
+        /*var musica = jogador.GetComponent<AudioSource>();
+        musica.Pause();*/
+
+    }
+
+    public void Unpause()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+        gameIsPaused = false;
+    }
+}
