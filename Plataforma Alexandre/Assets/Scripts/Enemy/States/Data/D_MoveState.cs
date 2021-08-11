ee19cd8c45807b122f4ea2eff5fba2a9ff7af3eb
Lixandre <alexikedamucenic@gmail.com>
2021-08-11 15:04:02 -0300

    Simbionte 33
    Enemy State Machine

---
+++ Plataforma Alexandre/Assets/Scripts/Enemy/States/Data/D_MoveState.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName ="New Move State Data",menuName ="Data / State Data / Move State")]
+public class D_MoveState : ScriptableObject
+{
+    public float movementSpeed = 3f;
+}
