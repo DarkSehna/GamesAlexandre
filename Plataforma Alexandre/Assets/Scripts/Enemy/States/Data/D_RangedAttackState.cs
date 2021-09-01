9d898b3baf0ebf55739e83dc1b01e7182e71b38b
Lixandre <alexikedamucenic@gmail.com>
2021-09-01 15:03:26 -0300

    Simbionte 39
    Início Ranged Enemy.

---
+++ Plataforma Alexandre/Assets/Scripts/Enemy/States/Data/D_RangedAttackState.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Ranged Attack State Data", menuName = "Data / State Data / Ranged Attack State")]
+public class D_RangedAttackState : ScriptableObject
+{
+    public GameObject projectile;
+
+    public float projectileDamage = 10f;
+    public float projectileSpeed = 12f;
+    public float projectileTravelDistance = 5f;
+}
---
+++ Plataforma Alexandre/Assets/Scripts/Projectiles/Projectile.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    private AttackDetails attackDetails;
+    private float speed;
+    private float travelDistance;
+    private float xStartPos;
+
+    [SerializeField] private float gravity;
+    [SerializeField] private float damageRadius;
+
+    private Rigidbody2D rb;
+    private bool isGravityOn;
+    private bool hasHitGround;
+
+    [SerializeField] private LayerMask whatIsGround;
+    [SerializeField] private LayerMask whatIsPlayer;
+    [SerializeField] private Transform damagePosition;
+}
