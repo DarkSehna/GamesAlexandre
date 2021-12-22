f60779ddc2cdfd10475bc652188c672b3ef9c80e
Lixandre <alexikedamucenic@gmail.com>
2021-12-22 16:08:13 -0300

    Simbionte 54
    Ranged Weapon

---
+++ Plataforma Alexandre/Assets/Scripts/Scriptable Objects/Weapons/SO_RangedWeaponData.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Ranged Weapon Data", menuName = "Data/Weapon Data/Ranged Weapon")]
+public class SO_RangedWeaponData : SO_WeaponData
+{
+    public GameObject projectile;
+
+    public float projectileDamage = 10f;
+    public float projectileSpeed = 12f;
+    public float projectileTravelDistance = 5f;
+}
---
+++ Plataforma Alexandre/Assets/Scripts/Weapons/RangedWeapon.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RangedWeapon : Weapon
+{
+    protected SO_RangedWeaponData rangedWeaponData;
+    protected Projectile projectileScript;
+    public GameObject attackPosition;
+
+    public override void AnimationActionTrigger()
+    {
+        base.AnimationActionTrigger();
+
+        rangedWeaponData.projectile = GameObject.Instantiate(rangedWeaponData.projectile, attackPosition.transform.position, attackPosition.transform.rotation);
+        projectileScript = rangedWeaponData.projectile.GetComponent<Projectile>();
+        projectileScript.FireProjectile(rangedWeaponData.projectileSpeed, rangedWeaponData.projectileTravelDistance, rangedWeaponData.projectileDamage);
+    }
+}
