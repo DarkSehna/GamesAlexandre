82a743ef1bb317ec14793c0adb944ba051c3a543
Lixandre <alexikedamucenic@gmail.com>
2022-08-04 17:33:40 -0300

    Simbionte 70
    Começo do mapa da área vermelha; Debug do knockback de trap.

---
+++ Plataforma Alexandre/Assets/Scripts/Other/TrapDamage.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrapDamage : MonoBehaviour
+{
+    [SerializeField] int damage = 99;
+
+    private Player player;
+    
+    public void DamageEntity(IDamageable entity, float damage)
+    {
+        entity.Damage(damage);
+    }
+
+    public void KnockbackEntity(IKnockbackable entity, int facingDirection)
+    {
+        entity.Knockback(Vector2.one, 10f, -facingDirection);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        OnTriggerStay2D(collision);
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+
+        IDamageable damageable = collision.GetComponent<IDamageable>();
+        if(damageable != null)
+        {
+            DamageEntity(damageable, damage);
+        }
+
+        IKnockbackable knockbackable = collision.GetComponent<IKnockbackable>();
+        if(knockbackable != null && gameObject.tag != "Water")
+        {
+            player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
+            Debug.Log(player);
+            KnockbackEntity(knockbackable, player.Core.Movement.facingDirection);
+        }
+    }
+}
